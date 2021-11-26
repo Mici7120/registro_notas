2806bce5e7a6b420f514b9e54b54c2376de22c04
Mici7120 <christian.villanueva@correounivalle.edu.co>
2021-11-26 17:04:07 -0500

    primera subida de archivos en backend y frontend

---
+++ backEnd/src/test/java/com/example/registro_notas/RegistroNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registro_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
